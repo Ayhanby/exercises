3a341ebf4c29faee4c3294e925e3557d28e2ae95
Ayhanby <psikolik.3@gmail.com>
2019-11-29 06:15:43 -0600

    ok

--- ArtiEsittirOperatoru.java
+++ ArtiEsittirOperatoru.java
@@ -5,10 +5,7 @@ public class ArtiEsittirOperatoru {
 	public static void main(String[] args) {
 		String yazi = "abc";
 		
-		yazi += "de";
-		yazi += "de";
-		yazi += "de";
-		yazi += "de";
+		
 		
 		System.out.println(yazi);
 	}
