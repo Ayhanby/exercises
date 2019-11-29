0d30dd4778d9e89ced0d582ecff226123cda10f6
Ayhanby <psikolik.3@gmail.com>
2019-11-29 05:50:47 -0600

    folder1 added

---
+++ ArtiEsittirOperatoru copy.java
@@ -0,0 +1,16 @@
+package bolum05;
+
+public class ArtiEsittirOperatoru {
+
+	public static void main(String[] args) {
+		String yazi = "abc";
+		
+		yazi += "de";
+		yazi += "de";
+		yazi += "de";
+		yazi += "de";
+		
+		System.out.println(yazi);
+	}
+
+}
